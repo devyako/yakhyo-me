0200e867b85c630d1f7dbf9c77602a4fcdd2f4b1
devyako <yakhyoismoildjonov@gmail.com>
2020-04-15 19:01:48 +0500

    update

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,7 +4,32 @@ export default class Blog extends Component {
   render() {
     return (
       <Container>
-        <p>hi</p>
+        <div className="container">
+          
+          <div className="blog">
+            <div className="blog-cont">
+              <div className="time">Thursday, April 9th 2020 (5 days ago)</div>
+              <div className="title">Simpler Pricing</div>
+              <div className="article">
+                Since the launch of our platform, we have always aimed to make
+                our pricing model as simple as possible, and perfectly tailored
+                to your needs. Today, we are taking a giant leap towards that
+                goal by introducing our new pricing plans for your personal
+                account and teams.
+              </div>
+            </div>
+          </div>
+          <div className="recommended-blogs">
+            <h2>Recommended Posts</h2>
+            <ul>
+              <li>Simpler Test</li>
+              <li>Simpler Test</li>
+              <li>Simpler Test</li>
+              <li>Simpler Test</li>
+              <li>Simpler Test</li>
+            </ul>
+          </div>
+        </div>
       </Container>
     );
   }
